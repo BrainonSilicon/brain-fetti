811feacf84e88b868caaf38d21987ebffd057e7c
Alex Castillo <alex@castillo.io>
2020-02-11 11:40:36 -0500

    FIX: calm page subscription and default state

--- src/pages/Calm.js
+++ src/pages/Calm.js
@@ -1,35 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { Nav } from "../components/Nav";
 
-//import the Confetti component here
-import { TheConfetti } from "../components/Confetti/Confetti.js"; //does this need to be Confetti.js?
+import { TheConfetti } from "../components/Confetti/Confetti.js";
 
 export function Calm({ user, notion }) {
-  const [calm, setCalmScore] = useState();
+  const [calm, setCalmScore] = useState(0);
   const [showConfetti, setShowConfetti] = useState(false);
 
   useEffect(() => {
-    if (user && notion) {
-      //this might be made redundant by the above
-      notion.calm().subscribe(calm => {
-        const score = Number(calm.probability.toFixed(2));
-        setCalmScore(score);
-        if (score > 0.3) {
-          setShowConfetti(true);
-        } else {
-          setShowConfetti(false);
-        }
-      });
+    if (!user || !notion) {
+      return;
     }
+
+    const subscription = notion.calm().subscribe(calm => {
+      const score = Number(calm.probability.toFixed(2));
+      setCalmScore(score);
+      if (score > 0.3) {
+        setShowConfetti(true);
+      } else {
+        setShowConfetti(false);
+      }
+    });
+
+    return () => subscription.unsubscribe();
   }, [user, notion]);
 
-  //changed this and lifted from notion-ocean
   return (
     <main>
       <h1>Welcome.Try to stay calm!</h1>
-      {user ? <Nav notion={notion} /> : null}
-      <meter value={calm} min={0} max={1} />
-      <TheConfetti calm={calm} />
       {user ? <Nav notion={notion} /> : null}
       <div className="calm-score">
         &nbsp;{calm * 100}% <div className="calm-word test">Calm</div>
